9cfb2245c69bcae6f7df7ffd5f4215bd9a504737
mrabvn <121527734+mrabvn@users.noreply.github.com>
2023-03-13 17:09:51 +0700

    Delete bachngocsach directory

--- bachngocsach/src/home.js
+++
@@ -1,9 +0,0 @@
-load('host.js');
-function execute() {
-    return Response.success([
-        {title: "Truyện Dịch", input: HOST + "/reader/recent-bns", script: "tab.js"},
-        {title: "Truyện Convert", input: HOST + "https://truyenbns.com/reader/recent-cv", script: "tab.js"},
-        {title: "Sáng Tác", input: HOST + "/reader/recent-sangtac", script: "tab.js"},
-        {title: "Sưu tầm", input: HOST + "/reader/recent-st", script: "tab.js"}
-    ]);
-}
