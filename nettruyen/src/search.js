f31cb306a55388e36dffb546de424f04d8512358
mrabvn <121527734+mrabvn@users.noreply.github.com>
2023-03-13 17:02:35 +0700

    Update search.js

--- nettruyen/src/search.js
+++ nettruyen/src/search.js
@@ -1,7 +1,7 @@
 function execute(key, page) {
     if (!page) page = '1';
 
-    let response = fetch("https://www.nettruyen.live/tim-truyen", {
+    let response = fetch("https://www.nettruyenvi.com/tim-truyen", {
         method: "GET",
         queries: {"keyword": key, "page": page}
     });
@@ -21,7 +21,7 @@ function execute(key, page) {
                 link: e.select("h3 a").first().attr("href"),
                 cover: coverImg,
                 description: e.select(".chapter a").first().text(),
-                host: "https://www.nettruyen.live"
+                host: "https://www.nettruyenvi.com"
             });
         });
 
